74d84c03819d13a135da19488006f45fc0b3eaad
Tocher <tocher99@gmail.com>
2014-10-08 00:17:14 +0300

    lab 1.1

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
     app.get('/', require('./frontpage').get);
     app.get('/inverseMatrix', require('./inverseMatrix').get);
+    app.get('/SimplexMethod', require('./SimplexMethod').get);
 
     app.post('/inverseMatrix/calculate', require('./inverseMatrix').post);
 };
